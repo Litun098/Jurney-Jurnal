a75495031c1bca21ee12be48eb6f13fe14d22276
litun098 <nayaklitun9@gmail.com>
2024-05-03 11:04:43 +0530

    Added blog slice

--- src/store/blogSlice.js
+++ src/store/blogSlice.js
@@ -1,3 +1,18 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
-const blogSlice = 
+const initialState = {
+  blog: null,
+};
+
+const blogSlice = createSlice({
+  name: "blog",
+  initialState,
+  reducers: {
+    addBlog: (state, action) => {
+      state.blog = action.payload;
+    },
+  },
+});
+
+export const { addBlog } = blogSlice.actions;
+export default blogSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,13 @@
-import {configureStore} from '@reduxjs/toolkit';
-import authSlice from './authSlice';
+import { configureStore } from "@reduxjs/toolkit";
+
+import authSlice from "./authSlice";
+import blogSlice from "./blogSlice";
 
 const store = configureStore({
-    reducer: {
-        auth : authSlice,
-        //TODO: add more slices here for posts
-    }
+  reducer: {
+    auth: authSlice,
+    blog: blogSlice,
+  },
 });
-
 
 export default store;
